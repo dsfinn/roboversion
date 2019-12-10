e5e33f9ed28354451c43504c1c7162bd57c751cd
David Finn <dsfinn@gmail.com>
2019-12-10 22:31:38 +1100

    Implement Version as subclass of namedtuple

--- roboversion/version.py
+++ roboversion/version.py
@@ -5,6 +5,7 @@ Modelling of PEP440-compliant versions and components
 import enum
 import logging
 import re
+from collections import namedtuple
 from datetime import datetime
 from itertools import zip_longest
 
@@ -22,13 +23,13 @@ PEP440_EXPRESSION = re.compile(
 )
 
 
-class Version:
+_VERSION_COMPONENTS = (
+	'epoch', 'release', 'prerelease', 'post', 'dev', 'local')
+
+class Version(namedtuple('_Components', _VERSION_COMPONENTS)):
 	"""
 	A PEP440-compliant version
 	"""
-	__slots__ = (
-		'_epoch', '_release', '_prerelease', '_post', '_dev', '_local')
-
 	class PrereleaseCategory(enum.IntEnum):
 		"""
 		Types of prerelease versions
@@ -69,8 +70,8 @@ class Version:
 		PrereleaseCategory.RELEASE_CANDIDATE: 'rc',
 	}
 
-	def __init__(
-			self,
+	def __new__(
+			cls,
 			release,
 			*,
 			epoch=0,
@@ -79,32 +80,18 @@ class Version:
 			dev=None,
 			local=None,
 	):
-		"""
-		:param Release release: The release version
-		:param int epoch: The epoch
-		:param Prerelease prerelease: The prerelease version
-		:param int post: The postrelease version
-		:param int dev: The development version
-		:param str local: The local version string
-		"""
-		self._set_epoch(epoch)
-		self._set_release(release)
-		self._set_prerelease(prerelease)
-		self._set_post(post)
-		self._set_dev(dev)
-		self._set_local(local)
-
-	def __repr__(self):
-		return (
-			f'{self.__class__.__name__}'
-			f'(release={self.release!r},'
-			f' epoch={self.epoch!r},'
-			f' prerelease={self.prerelease!r},'
-			f' post={self.post!r},'
-			f' dev={self.dev!r},'
-			f' local={self.local!r}'
-			f')'
-		)
+		cls._check_nonnegative_integer(epoch)
+		release = cls._build_release(release)
+		if prerelease is not None:
+			prerelease = cls._build_prerelease(prerelease)
+		if post is not None:
+			cls._check_nonnegative_integer(post)
+		if dev is not None:
+			cls._check_nonnegative_integer(dev)
+		if local is not None:
+			local = cls._build_local(local)
+		return super().__new__(
+			cls, epoch, release, prerelease, post, dev, local)
 
 	def __str__(self):
 		strings = []
@@ -121,30 +108,6 @@ class Version:
 		if self.local:
 			strings.append(f'+{self.local}')
 		return ''.join(strings)
-
-	@property
-	def epoch(self):
-		return self._epoch
-
-	@property
-	def release(self):
-		return self._release
-
-	@property
-	def prerelease(self):
-		return self._prerelease
-
-	@property
-	def post(self):
-		return self._post
-
-	@property
-	def dev(self):
-		return self._dev
-
-	@property
-	def local(self):
-		return self._local
 
 	def get_bumped(self, index=-1, increment=1):
 		"""
@@ -192,7 +155,7 @@ class Version:
 		if not match:
 			raise ValueError(
 				f'{string!r} is not a PEP440-compliant public version string')
-		release = tuple(int(x) for x in match['release'].split('.'))
+		release = (int(x) for x in match['release'].split('.'))
 		optionals = {}
 		if match['epoch']:
 			optionals['epoch'] = int(match['epoch'])
@@ -214,56 +177,40 @@ class Version:
 			optionals['local'] = match['local']
 		return cls(release=release, **optionals)
 
-	def _set_epoch(self, epoch):
-		if epoch < 0:
-			raise ValueError('epoch must be a non-negative integer')
-		self._epoch = int(epoch)
+	@classmethod
+	def _check_nonnegative_integer(cls, value):
+		if value != int(value):
+			raise TypeError(f'{value!r} is not a non-negative integer')
+		if value < 0:
+			raise ValueError(f'{value!r} is not a non-negative integer')
 
-	def _set_release(self, release):
-		release = tuple(release)
+	@classmethod
+	def _build_release(cls, iterable):
+		release = tuple(iterable)
 		if not release:
 			raise ValueError('Release components cannot be empty')
 		bad_values = []
 		for index, component in enumerate(release):
-			if component < 0:
+			try:
+				cls._check_nonnegative_integer(component)
+			except (TypeError, ValueError):
 				bad_values.append(f'{component!r} (index {index})')
 		if bad_values:
 			raise ValueError(
 				'Release components must consist of non-negative integers;'
 				f' bad values are {", ".join(bad_values)}')
-		self._release = tuple(int(x) for x in release)
+		return release
 
-	def _set_prerelease(self, prerelease):
-		if prerelease is None:
-			self._prerelease = None
-			return
-		category, value = prerelease
-		category = self.PrereleaseCategory(category)
-		if value < 0:
-			raise ValueError('Prerelease value must be a non-negative integer')
-		self._prerelease = (category, int(value))
+	@classmethod
+	def _build_prerelease(cls, iterable):
+		category, value = iterable
+		cls.PrereleaseCategory(category)
+		cls._check_nonnegative_integer(value)
+		return category, value
 
-	def _set_post(self, post):
-		if post is None:
-			self._post = None
-			return
-		if post < 0:
-			raise ValueError('post must be a non-negative integer')
-		self._post = int(post)
-
-	def _set_dev(self, dev):
-		if dev is None:
-			self._dev = None
-			return
-		if dev < 0:
-			raise ValueError('dev must be a non-negative integer')
-		self._dev = int(dev)
-
-	def _set_local(self, local):
-		if local is None:
-			self._local = None
-			return
-		if not self.LOCAL_VERSION_EXPRESSION.fullmatch(local):
+	@classmethod
+	def _build_local(cls, local):
+		if not cls.LOCAL_VERSION_EXPRESSION.fullmatch(local):
 			raise ValueError(
 				f'{local!r} is not a valid local version specifier')
-		self._local = '.'.join(self._SEPARATOR_EXPRESSION.split(local))
+		return '.'.join(cls._SEPARATOR_EXPRESSION.split(local))
